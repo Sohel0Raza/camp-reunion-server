b00963efa86c24b299624ea7b88432c033a5eaa7
soheldot <soheldot1239@gmail.com>
2023-06-10 12:22:21 +0600

    select class data load and post

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const classesCollection = client.db('campReunionDB').collection('classes')
+    const selectCollection = client.db('campReunionDB').collection('selectClass')
 
     //classes 
     app.get('/allClass', async(req, res)=>{
@@ -40,6 +41,30 @@ async function run() {
         }
         const result = await classesCollection.find(query,options).limit(6).toArray()
         res.send(result)
+    })
+    app.get('/populerInstructor', async(req, res)=>{
+        const query = {}
+        const options ={
+            sort:{"available_seats": -1}
+        }
+        const result = await classesCollection.find(query,options).limit(6).toArray()
+        res.send(result)
+    })
+
+    //select classes
+    app.get('/selectClass', async(req,res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }
+      const query = {email: email}
+      const result = await selectCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.post('/selectClass', async(req, res) =>{
+      const selectClass = req.body;
+      const result = await selectCollection.insertOne(selectClass)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
