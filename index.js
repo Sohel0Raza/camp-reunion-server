7fe420e8661ac3b7781d55d78c0c382c971007d3
soheldot <soheldot1239@gmail.com>
2023-06-10 20:13:23 +0600

    make amin and instructor route api set up complete

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middelwire
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q6zwl04.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,57 +17,115 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const classesCollection = client.db('campReunionDB').collection('classes')
-    const selectCollection = client.db('campReunionDB').collection('selectClass')
 
-    //classes 
-    app.get('/allClass', async(req, res)=>{
-        const result = await classesCollection.find().toArray()
-        res.send(result)
-    })
-    app.get('/populerClass', async(req, res)=>{
-        const query = {}
-        const options ={
-            sort:{"available_seats": -1}
-        }
-        const result = await classesCollection.find(query,options).limit(6).toArray()
-        res.send(result)
-    })
-    app.get('/populerInstructor', async(req, res)=>{
-        const query = {}
-        const options ={
-            sort:{"available_seats": -1}
-        }
-        const result = await classesCollection.find(query,options).limit(6).toArray()
-        res.send(result)
-    })
-
-    //select classes
-    app.get('/selectClass', async(req,res)=>{
-      const email = req.query.email;
-      if(!email){
-        res.send([])
+    const classesCollection = client.db("campReunionDB").collection("classes");
+    const selectCollection = client
+      .db("campReunionDB")
+      .collection("selectClass");
+    const usersCollection = client.db("campReunionDB").collection("users");
+    // user related api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user alrady exists" });
       }
-      const query = {email: email}
-      const result = await selectCollection.find(query).toArray()
-      res.send(result)
-    })
-    app.post('/selectClass', async(req, res) =>{
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    //classes related api
+    app.get("/allClass", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/populerClass", async (req, res) => {
+      const query = {};
+      const options = {
+        sort: { available_seats: -1 },
+      };
+      const result = await classesCollection
+        .find(query, options)
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/populerInstructor", async (req, res) => {
+      const query = {};
+      const options = {
+        sort: { available_seats: -1 },
+      };
+      const result = await classesCollection
+        .find(query, options)
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    //select classes related api
+    app.get("/selectClass", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await selectCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/selectClass", async (req, res) => {
       const selectClass = req.body;
-      const result = await selectCollection.insertOne(selectClass)
-      res.send(result)
-    })
+      const result = await selectCollection.insertOne(selectClass);
+      res.send(result);
+    });
+
+    app.delete("/selectClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -77,9 +133,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('CampReunion is running...........')
-})
-app.listen(port,()=>{
-    console.log(`CampReunio is running on port:${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("CampReunion is running...........");
+});
+app.listen(port, () => {
+  console.log(`CampReunio is running on port:${port}`);
+});
